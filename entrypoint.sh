cbaa019eae4fe5871345a0624769307f118a6a7a
David Parrish <daveparrish@tutanota.com>
2020-05-15 11:47:52 -0400

    Allow entrypoint.sh to work with UID 0 and GID 0 for root users

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,30 @@
 #!/bin/sh
 set -e
 
-_UID=$(id -u joinmarket)
-_GID=$(id -g joinmarket)
+UID=${UID:-1000}
+GID=${GID:-1000}
 
-# shellcheck disable=SC2039
-if [ -n "$UID" ] && [ "$UID" != "$_UID" ]; then
-    _UID="$UID"
-    usermod -u "$_UID" joinmarket
+mkdir -p /home/joinmarket/.joinmarket
+
+if [ "$UID" -eq 0 ]; then
+    chown -R "$_UID":"$_GID" /home/joinmarket/.joinmarket
+    exec bash -c "cd /jm/clientserver/scripts && python3 $*"
+else
+    _UID=$(id -u joinmarket)
+    _GID=$(id -g joinmarket)
+
+    if [ -n "$UID" ] && [ "$UID" != "$_UID" ]; then
+        _UID="$UID"
+        usermod -u "$_UID" joinmarket
+    fi
+
+    if [ -n "$GID" ] && [ "$GID" != "_GID" ]; then
+        _GID="$GID"
+        groupmod -g "$_GID" joinmarket
+    fi
+
+    # Fix permissions according to the host user UID and GID
+    chown -R "$_UID":"$_GID" /home/joinmarket/.joinmarket
+
+    exec gosu joinmarket bash -c "cd /jm/clientserver/scripts && python3 $*"
 fi
-
-# shellcheck disable=SC2153
-if [ -n "$GID" ] && [ "$GID" != "_GID" ]; then
-    _GID="$GID"
-    groupmod -g "$_GID" joinmarket
-fi
-
-# Fix permissions according to the host user UID and GID
-chown -R "$_UID":"$_GID" /home/joinmarket/.joinmarket
-
-exec gosu joinmarket bash -c "cd /jm/clientserver/scripts && python3 $*"
